47af8eb9315f6fdd0354cd1f407000ad6ba11d98
Yaroslav <y.surilov@infomir.com>
2016-09-08 15:43:56 +0300

    remove init page from all locations

--- src/js/main.js
+++ src/js/main.js
@@ -11,12 +11,8 @@ var app = require('mag-app');
 app.once('load', function () {
     // load pages
     app.pages = {
-        init: require('./pages/init'),
         main: require('./pages/main')
     };
-
-    // show splash screen
-    //app.route(app.pages.init);
 
     // show main page
     app.route(app.pages.main);
