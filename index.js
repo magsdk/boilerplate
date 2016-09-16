2a52d57ce65c0ed17fdbc0a33fd4be175f8657f6
Yaroslav <y.surilov@infomir.com>
2016-09-16 17:32:54 +0300

    fix deps

--- index.js
+++ index.js
@@ -5,11 +5,10 @@
 'use strict';
 
 // redefinition of plugin option example
-require('spa-plugin-jade/config').release.target = 'app/index.html';
 
 // load default plugins
 // and run default tasks
-module.exports = require('stbsdk/default');
+module.exports = require('magsdk/default');
 
 // redefinition of default task example
 // module.exports.runner.task('default', module.exports.runner.serial('build'));
