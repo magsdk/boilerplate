115f30a4caf97ff2cf1a20a564ea65f8f388ae6a
Yaroslav <y.surilov@infomir.com>
2016-09-22 14:50:57 +0300

    add gettext loading

--- src/js/main.js
+++ src/js/main.js
@@ -9,11 +9,13 @@ var app = require('mag-app');
 
 // everything is ready
 app.once('load', function () {
-    // load pages
-    app.pages = {
-        main: require('./pages/main')
-    };
+    require('spa-gettext').load({name: core.environment.language}, function () {
+        // load pages
+        app.pages = {
+            main: require('./pages/main')
+        };
 
-    // show main page
-    app.route(app.pages.main);
+        // show main page
+        app.route(app.pages.main);
+    });
 });
