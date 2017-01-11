1c0b2d38bce5a738e235c7cac2f9449253a97591
Stanislav Kalashnik <darkpark.main@gmail.com>
2017-01-11 17:20:45 +0200

    add gettext plugin commented example

--- index.js
+++ index.js
@@ -5,6 +5,10 @@
 'use strict';
 
 // redefinition of plugin option example
+// require('spa-plugin-static/config').default.port = 8000;
+
+// activate gettext languages
+// require('spa-plugin-gettext/config').default.languages = ['ru', 'fr'];
 
 // load default plugins
 // and run default tasks
